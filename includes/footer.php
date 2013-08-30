616decbab01dc531e5471ab8f43d0689880b6a7f
Pawel Szczurko <pszczurko@gmail.com>
2013-08-29 21:26:49 -0400

    made images folder and created footer/header

---
+++ includes/footer.php
@@ -0,0 +1,14 @@
+<div class="socialLinks">
+				<a href="https://github.com/pawelSzu"><img src="images/github.jpg" /></a>
+				<a href="http://www.linkedin.com/pub/pawel-szczurko/67/666/7a4"><img src="images/linkedin.gif" /></a>
+			</div>
+		</div>
+
+	</div>
+
+
+</div>
+
+<script src="includes/scripts.js"></script>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+  <head>
+  	<meta charset="utf-8">
+  	<meta name="viewport" content="width=700">
+  	<title>Pawe&#322 Szczurko</title>
+	<meta name="description" content="">
+	<meta name="author" content="Pawel Szczurko">
+	<link rel="icon" href="images/favicon.ico" type="image/x-icon" />
+  	<link href='http://fonts.googleapis.com/css?family=Roboto+Slab' rel='stylesheet' type='text/css'>
+    <link href="includes/normalize.css" rel="stylesheet" type="text/css">
+    <link href="includes/styles.css" rel="stylesheet" type="text/css">
+  </head>
+ 
+<body>
+  
+<div id="container">
+	<div id="header" >
+		<div class="headerText">
+				<h1 style="margin-bottom:0;">Pawe&#322 Szczurko</h1>
+				<h2>Rutgers University - New Brunswick, NJ</h2>
+		</div>
+		<div class="headerImg">
+				<img class="pawelImg" src="images/pawel.gif" />
+		</div>
+	</div>
+	<div id="content">
+		<div class="chainMenu">
+			<div class="chainHolder"><img src="images/chain_s.gif" class="chainImg" /></div>
+			<div class="chainHolder"><img src="images/chain_s.gif" class="chainImg" /></div>
+			<div class="chainHolder"><img src="images/chain_s.gif" class="chainImg" /></div>
+			<div class="chainHolder"><img src="images/chain_s.gif" class="chainImg" /></div>
+			<div class="chainHolder"><img src="images/chain_s.gif" class="chainImg" /></div>
+
+			<div class="signHolder">
+				<div id="sign" class="" onMouseOver="animateThis(this);" >
+					<a href="index.html"><img class="signImg" src="images/aboutme_y.gif" /></a>
+				</div>
+			</div>
+
+			<div class="signHolder">
+				<div id="sign" class="" onMouseOver="animateThis(this);" >
+					<a href="http://www.google.com"><img class="signImg" src="images/work_y.gif" /></a>
+				</div>
+			</div>
+
+			<div class="signHolder">
+				<div id="sign" class="" onMouseOver="animateThis(this);" >
+					<a href="http://www.google.com"><img class="signImg" src="images/projects_y.gif" /></a>
+				</div>
+			</div>
+			<div class="signHolder">
+				<div id="sign" class="" onMouseOver="animateThis(this);" >
+					<a href="szczurko_res2.pdf" target="_blank"><img class="signImg" src="images/resume_y.gif" /></a>
+				</div>
+			</div>
+			<div class="signHolder">
+				<div id="sign" class="" onMouseOver="animateThis(this);" >
+					<a href="http://www.google.com"><img class="signImg" src="images/contact_y.gif" /></a>
+				</div>
+			</div>
+		</div>
+		<div id="contentHolder">
