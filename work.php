ab0ea20e781d3e1324af9d2172a2feae85f52df0
Pawel Szczurko <pszczurko@gmail.com>
2013-08-29 23:54:07 -0400

    finished work.php, should be done with the entire site for now

--- work.php
+++ work.php
@@ -3,9 +3,35 @@ include ('includes/header.php');
 ?>
 			Work Experience<div class="hr"></div>
 			<div class="workBox">
+				<img src="images/gsapp.png" class="workImgNoBottom">
+				<a href="http://gsappweb.rutgers.edu" target="_blank"><i> 
+				Rutgers Graduate School of Applied &amp; Prof. Psychology</i></a>: Web Dev<br>
+				As the lone web developer I maintain a PHP/MySQL system used by students, faculty, and staff of GSAPP. 
+				The system stores student records as well as allows users to reserve rooms, fill out various surverys, 
+				and view various reports. For the most part my job consits of making modifications/additions to the site 
+				from anything as small as adding a textbox to creating surveys for students to fill out and generate
+				reports based on the survey results. Recently I completed an integration of an outside system (
+				<a href="http://www.scholarchip.com/">ScholarChip</a>) into our system so that users could register 
+				on our site for a conference and then be redirected to <a href="http://www.scholarchip.com/">ScholarChip</a>
+				, which would process their online credit card payment.
+			</div>
+			<div class="workBox">
+				<img src="images/rutgers.png" class="workImgNoBottom">
+				<a href="http://oit.rutgers.edu" target="_blank"><i> Rutgers Office of Information Technology</i></a>: 
+				IT Assistant<br>
+				This was my first On-Campus job at Rutgers. As a part of the Work-Study program at Rutgers I was
+				assigned to one of the IT groups. I worked there for the majority of my freshman year. While there, I
+				researched for various replacement hardware (such as printers), imaged a large quanity of computers for
+				employees using GhostCast, and did minor web development work.
+			</div>
+			<div class="workBox">
 				<img src="images/krk.jpg" class="workImgNoBottom">
 				<a href="http://www.krakowiakrestaurant.com" target="_blank"><i> Krakowiak Restaurant</i></a>: Web Dev<br>
-				I was given the task of creating a website for a local Polish restaurant. The only guidence I was given was, <i>'We need a website, here's our menu'</i>.
+				I was given the task of creating a website for a local Polish restaurant. The only guidence I was 
+				given was, <i>'We need a website, here's our menu'</i>. Despite having no artistic nor design 
+				skill whatsoever, I somehow managed to patch up a decently looking site. During 2012-2013 winter break
+				I added a back-end administrative side to the page which allows for simple and dynamic editing of the menu. 
+				I still occasionally make little uptades to the site whenever I'm asked.
 			</div>
 			<div class="workBox">
 				<img src="images/system.jpg" class="workImgNoBottom">
