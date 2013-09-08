2e9378c21ef99ba59a4d40fa7ecaffcfcecaafc6
Pawel Szczurko <pszczurko@gmail.com>
2013-09-08 15:40:44 -0400

    update github link

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
 			<div class="socialLinks">
-				<a href="https://github.com/pawelSzu"><img src="images/github.jpg" /></a>
+				<a href="https://github.com/ps"><img src="images/github.jpg" /></a>
 				<a href="http://www.linkedin.com/pub/pawel-szczurko/67/666/7a4"><img src="images/linkedin.gif" /></a>
 			</div>
 		</div>
