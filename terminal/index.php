eeb1366cdc2f1d4703347ebd10ffb89eac5b2660
Pawel Szczurko <pszczurko@gmail.com>
2014-03-09 20:21:40 -0400

    added a link to the terminal through subdirectory structure

---
+++ terminal/index.php
@@ -0,0 +1,6 @@
+<?php 
+header('Location: http://terminal.pawel.pw');
+die();
+?>
+If you have not been redirected to the proper location then the live
+demo of the terminal can be found at <a href="http://terminal.pawel.pw">http://terminal.pawel.pw</a>.
